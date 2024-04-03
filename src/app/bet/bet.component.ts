18735732d231e9289593c867f0dd3edb19c3a13f
hamizannordin <hamizan.nordin@gmail.com>
2024-04-03 16:48:57 +0800

    Customize bet module div

--- src/app/bet/bet.component.ts
+++ src/app/bet/bet.component.ts
@@ -4,8 +4,21 @@ import { BetItem } from 'src/model/betItem';
 @Component({
   selector: 'app-bet',
   templateUrl: './bet.component.html',
-  styleUrls: ['./bet.component.css', '../app.component.css']
+  styleUrls: ['./bet.component.css', '../app.component.css', '../../styles.css']
 })
 export class BetComponent {
-  betList: BetItem[] = [];
+  betList: BetItem[] = [
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'), 
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678'),
+    // new BetItem('', '12345678')
+  ];
 }
