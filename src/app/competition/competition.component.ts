b21bb4d6e2845d7607605e008d4403c4c20d9413
hamizannordin <hamizan.nordin@gmail.com>
2024-04-17 15:21:35 +0800

    Add create/reset competition function

--- src/app/competition/competition.component.ts
+++ src/app/competition/competition.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CompetitionService } from '../../service/competition/competition.service';
 
 @Component({
   selector: 'app-competition',
   templateUrl: './competition.component.html',
   styleUrls: ['./competition.component.css', '../../styles.css']
 })
-export class CompetitionComponent {
-  competitionName = crypto.randomUUID();
+export class CompetitionComponent implements OnInit {
+  competitionName: string = crypto.randomUUID();
+  competitionId: string | undefined;
+  competitionService: CompetitionService;
+
+  constructor(competitionService: CompetitionService) {
+    this.competitionService = competitionService;
+    this.createNewCompetition();
+
+  }
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+  }
 
   resetCompetition() {
     this.competitionName = crypto.randomUUID();
-    console.log(this.competitionName);
+    this.createNewCompetition();
+  }
+
+  async createNewCompetition() {
+    try {
+      await this.competitionService.createCompetition(this.competitionName)
+        .then((response) => {
+          //console.log(response);
+          this.competitionId = response;
+        })
+        .catch((reason) => {
+          console.error(reason);
+        });
+      console.log('competition id: ', this.competitionId);
+    }
+    catch (error) {
+      console.error(error);
+    }
   }
 }
