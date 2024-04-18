a8035f0c1f5892283c918edf32e83602fe3afca8
hamizannordin <hamizan.nordin@gmail.com>
2024-04-18 14:47:51 +0800

    Add bet service

---
+++ src/service/bet/bet.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../api.service';
+import { BetItem } from 'src/model/betItem';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BetService {
+
+  constructor(private api: ApiService) { }
+
+  async createBet(betItem: BetItem): Promise<any> {
+    return new Promise(resolve => {
+      this.api.apiPost(environment.API_URL + '/bet/detail', betItem)
+        .then(response => { resolve(response); })
+        .catch(reason => { console.error(reason); })
+    });
+  }
+
+  async createRandomBet(total: string): Promise<string[]> {
+    return new Promise(resolve => {
+      this.api.apiGet(environment.API_URL + '/bet/random/combination?total=' + total)
+        .then(response => { resolve(response); })
+        .catch(reason => { console.error(reason) });
+    });
+  }
+}
