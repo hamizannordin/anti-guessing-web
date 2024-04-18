8c07128cc407db10f12229d08c7b6c321e87779a
hamizannordin <hamizan.nordin@gmail.com>
2024-04-18 14:55:05 +0800

    Add create bet function

--- src/app/bet/bet.component.ts
+++ src/app/bet/bet.component.ts
@@ -1,24 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BetItem } from 'src/model/betItem';
+import { BetService } from 'src/service/bet/bet.service';
+import { CompetitionService } from 'src/service/competition/competition.service';
 
 @Component({
   selector: 'app-bet',
   templateUrl: './bet.component.html',
   styleUrls: ['./bet.component.css', '../app.component.css', '../../styles.css']
 })
-export class BetComponent {
-  betList: BetItem[] = [
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'), 
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678'),
-    // new BetItem('', '12345678')
-  ];
+export class BetComponent implements OnInit {
+  betList: string[] = [];
+  inputBox: string = '';
+  totalBox: string = '';
+
+  constructor(private betService: BetService, private competitionService: CompetitionService) { }
+  ngOnInit(): void {
+    this.competitionService.currentFlag.subscribe(flag => {
+      if(flag)
+        this.clearBet();
+    });
+  }
+
+  clearBet() {
+    this.betList = [];
+  }
+
+  createBet() {
+    let competitionId = this.competitionService.getCompetitionId();
+    let combinationInput = this.inputBox;
+    let betItem = new BetItem(competitionId, [combinationInput]);
+    //console.log(betItem);
+    this.betService.createBet(betItem)
+      .then(response => {
+        //console.log(response);
+        this.betList.push(combinationInput);
+      }).catch(reason => { console.error(reason); });
+    this.inputBox = '';
+  }
+
+  createRandomBet() {
+    let competitionId = this.competitionService.getCompetitionId();
+    this.betService.createRandomBet(this.totalBox)
+      .then(response => {
+        //console.log(response);
+        response.forEach(combination => {
+          this.betList.push(combination);
+        });
+        let betItem = new BetItem(competitionId, response);
+        this.betService.createBet(betItem)
+          .then().catch(reason => { console.error(reason); });
+      })
+      .catch(reason => { console.error(reason); });
+  this.totalBox = '';
+  }
 }
