4bec948cc09701eeef16c33b13ce68b120326f43
hamizannordin <hamizan.nordin@gmail.com>
2024-06-11 15:37:47 +0800

    Add search bet

--- src/service/bet/bet.service.ts
+++ src/service/bet/bet.service.ts
@@ -10,7 +10,7 @@ export class BetService {
 
   constructor(private api: ApiService) { }
 
-  async createBet(betItem: BetItem): Promise<any> {
+  async createBet(betItem: BetItem): Promise<BetItem> {
     return new Promise(resolve => {
       this.api.apiPost(environment.API_URL + '/bet/detail', betItem)
         .then(response => { resolve(response); })
@@ -23,6 +23,14 @@ export class BetService {
       this.api.apiGet(environment.API_URL + '/bet/random/combination?total=' + total)
         .then(response => { resolve(response); })
         .catch(reason => { console.error(reason) });
+    });
+  }
+
+  async searchBetCombination(competitionId: string, combination: string): Promise<BetItem[]> {
+    return new Promise(resolve => {
+      this.api.apiGet(environment.API_URL + '/bet/search?competition-id=' + competitionId + '&combination=' + combination)
+        .then(response => resolve(response))
+        .catch(reason => console.error(reason));
     });
   }
 }
